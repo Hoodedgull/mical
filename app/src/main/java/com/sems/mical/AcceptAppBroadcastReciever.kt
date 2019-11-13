2510eaa764a757ec7c44621caada82e0f748d457
Markus <mabar16@student.sdu.dk>
2019-11-13 11:19:32 +0100

    Accept/deny Intent

--- app/src/main/java/com/sems/mical/AcceptAppBroadcastReciever.kt
+++ app/src/main/java/com/sems/mical/AcceptAppBroadcastReciever.kt
@@ -13,26 +13,54 @@ import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.content.ContextCompat.getSystemService
 import android.widget.Toast
-
-
-
+import com.sems.mical.data.AppDatabase
+import com.sems.mical.data.entities.App
+import com.sems.mical.data.entities.MicrophoneIsBeingUsed
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 
 class AcceptAppBroadcastReciever : BroadcastReceiver() {
     private var count = 38;
     override fun onReceive(p0: Context?, p1: Intent?) {
+        Log.e("BBBB", "I AM RECEIVED")
+        val action = p1?.getStringExtra("action")
+        if (action == "accept") {
+            acceptRequest(p0, p1)
+            val toast = Toast.makeText(
+                p0?.applicationContext,
+                "This is a message displayed in an accepted Toast",
+                Toast.LENGTH_SHORT
+            )
 
-        val toast = Toast.makeText(
-            p0?.applicationContext,
-            "This is a message displayed in a Toast",
-            Toast.LENGTH_SHORT
-        )
+            toast.show()
 
-        toast.show()
+            Log.e("BBBB", "ACCEPTED APPPPPPPPPP")
+        } else if (action == "decline") {
+            declineRequest(p0, p1)
+            val toast = Toast.makeText(
+                p0?.applicationContext,
+                "This is a message displayed in a declined Toast",
+                Toast.LENGTH_SHORT
+            )
 
-        Log.e("BBBB", "ACCEPTED APPPPPPPPPP")
+            toast.show()
 
+            Log.e("BBBB", "DECLINED!!! APPPPPPPPPP")
+        }
 
+        val apps = AppDatabase.getInstance(p0!!)!!.appDao().getApps();
+        Log.e("HELP", apps.size.toString())
+    }
+
+    private fun acceptRequest(p0: Context?, p1: Intent?){
+        val appName = p1!!.getStringExtra("appname")
+        AppDatabase.getInstance(p0!!)!!.appDao().insertApp(App(appName, LocalDateTime.now().toString(), true, LocalDateTime.now().toString()))
+    }
+
+    private fun declineRequest(p0: Context?, p1: Intent?){
+        val appName = p1!!.getStringExtra("appname")
+        AppDatabase.getInstance(p0!!)!!.appDao().insertApp(App(appName, LocalDateTime.now().toString(), false, LocalDateTime.now().toString()))
     }
 
 
--- app/src/main/java/com/sems/mical/MicMonitoringService.kt
+++ app/src/main/java/com/sems/mical/MicMonitoringService.kt
@@ -3,10 +3,12 @@ package com.sems.mical
 import android.app.IntentService
 import android.app.Notification
 import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.room.Room
 import com.sems.mical.data.AppDatabase
 import com.sems.mical.data.entities.App
 import com.sems.mical.data.entities.MicrophoneIsBeingUsed
@@ -14,6 +16,10 @@ import sensorapi.micapi.MicUsedImpl
 import java.time.LocalDateTime
 
 class MicMonitoringService() : IntentService("Bob") {
+    companion object {
+        val context = this
+    }
+
     var cnt = 1
     override fun onHandleIntent(p0: Intent?) {
         Log.e("AAAA", "Got into handleIntent")
@@ -25,13 +31,31 @@ class MicMonitoringService() : IntentService("Bob") {
 
 
             val acceptIntent = Intent(this, AcceptAppBroadcastReciever::class.java).apply {
-                action = "what is an action"
+                action = "com.sems.mical.micallow"
                 putExtra(Notification.EXTRA_NOTIFICATION_ID, 123456789)
-            }
+                putExtra("action", "accept");
+                putExtra("appname", response.appName);
+                sendBroadcast(this)
+            } b
+
 
 
             val acceptPendingIntent: PendingIntent =
                 PendingIntent.getBroadcast(this, 123, acceptIntent, 0)
+
+            val declineIntent = Intent(this, AcceptAppBroadcastReciever::class.java).apply {
+                action = "com.sems.mical.micallow"
+                putExtra(Notification.EXTRA_NOTIFICATION_ID, 123456789)
+                putExtra("action", "decline");
+                putExtra("appname", response.appName);
+                sendBroadcast(this)
+            }
+
+
+
+            val declinePendingIntent: PendingIntent =
+                PendingIntent.getBroadcast(this, 123, declineIntent, 0)
+
 
 
             var builder = NotificationCompat.Builder(this, "hello")
@@ -39,7 +63,8 @@ class MicMonitoringService() : IntentService("Bob") {
                 .setContentTitle(response.appName)
                 .setContentText("Wants to use the mic")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .addAction(R.drawable.ic_stat_onesignal_default,"accept", acceptPendingIntent);
+                .addAction(R.drawable.ic_stat_onesignal_default, "Accept",acceptPendingIntent)
+                .addAction(R.drawable.ic_stat_onesignal_default, "Decline", declinePendingIntent);
 
             startForeground(123456789,builder.build())
 
